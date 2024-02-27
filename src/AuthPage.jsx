0c38b50667cd3f298172d27e023a87333299b64f
hjaishan <hemnath.j1995@gmail.com>
2024-02-27 11:06:39 +0530

    changes

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -1,86 +1,82 @@
 import { useState } from "react";
 import axios from "axios";
 
-
 const AuthPage = (props) => {
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
-    const [passwordConfirm, setPasswordConfirm] = useState();
-    const [email, setEmail] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirm, setPasswordConfirm] = useState();
+  const [email, setEmail] = useState();
 
-    const onLogin = (e) => {
-        e.preventDefault();
-        console.log(email);
-        console.log(password);
-        axios
-          .post("http://127.0.0.1:3000/api/v1/users/login", { email, password })
-          .then((r) => props.onAuth({ ...r.data, password })) // NOTE: over-ride secret
-          .catch((e) => console.log(JSON.stringify(e.response.data)));
-      };
+  const onLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:3000/api/v1/users/login", { email, password })
+      .then((r) => props.onAuth({ ...r.data, password })) // NOTE: over-ride secret
+      .catch((e) => console.log(JSON.stringify(e.response.data)));
+  };
 
+  const onSignUp = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:3000/api/v1/users/signup", {
+        name: username,
+        email: email,
+        password: password,
+        passwordConfirm: passwordConfirm,
+      })
+      .then((r) => props.onAuth({ ...r.data }))
+      .catch((err) => console.log(err));
+  };
 
-    const onSignUp = (e) => {
-      e.preventDefault();
-      axios.post(
-        'http://127.0.0.1:3000/api/v1/users/signup', {
-            name:username,
-            email:email,
-            password:password,
-            passwordConfirm:passwordConfirm,
-        })
-        .then( r => props.onAuth({ ...r.data }))
-        .catch(err => console.log(err))
-    };
-  
-    return (
-        <div className="login-page">
-          <div className="card">
-            {/* Login Form */}
-            <form onSubmit={onLogin}>
-              <div className="title">Login</div>
-              <input
-                type="text"
-                name="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit">LOG IN</button>
-            </form>
-    
-            {/* Sign Up Form */}
-            <form onSubmit={onSignUp}>
-              <div className="title">or Sign Up</div>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                type="text"
-                name="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <input
-                type="password"
-                name="passwordConfirm"
-                placeholder="Password"
-                onChange={(e) => setPasswordConfirm(e.target.value)}
-              />
-              {/* <input
+  return (
+    <div className="login-page">
+      <div className="card">
+        {/* Login Form */}
+        <form onSubmit={onLogin}>
+          <div className="title">Login</div>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+
+        {/* Sign Up Form */}
+        <form onSubmit={onSignUp}>
+          <div className="title">or Sign Up</div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            name="passwordConfirm"
+            placeholder="Password"
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+          {/* <input
                 type="text"
                 name="first_name"
                 placeholder="First name"
@@ -92,19 +88,19 @@ const AuthPage = (props) => {
                 placeholder="Last name"
                 onChange={(e) => setLastName(e.target.value)}
               /> */}
-              <button type="submit">SIGN UP</button>
-            </form>
-          </div>
-    
-          <style>{`
+          <button type="submit">SIGN UP</button>
+        </form>
+      </div>
+
+      <style>{`
           .login-page { width: 100vw; height: 100vh; padding-top: 6vw; background: linear-gradient(180deg, rgba(117,84,160,1) 7%, rgba(117,84,160,1) 17%, rgba(106,95,168,1) 29%, rgba(99,103,174,1) 44%, rgba(87,116,184,1) 66%, rgba(70,135,198,1) 83%, rgba(44,163,219,1) 96%, rgba(22,188,237,1) 100%, rgba(0,212,255,1) 100%); }
           .card { width: 200px; position: relative; left: calc(50vw - 100px); text-align: center; }
           .title { padding-top: 32px; font-size: 22px; color: white; font-weight: 700; }
           input { width: calc(100% - 16px); margin-top: 12px; padding: 8px; background-color: #e6f7ff; outline: none; border: 1px solid #e6f7ff; }
           button { margin-top: 12px; width: 100%; padding: 8px; }
           `}</style>
-        </div>
-      );
-    };
-    
-    export default AuthPage;
+    </div>
+  );
+};
+
+export default AuthPage;
--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,15 +1,32 @@
-import { PrettyChatWindow } from 'react-chat-engine-pretty';
+// import { PrettyChatWindow } from "react-chat-engine-pretty";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
 const ChatsPage = (props) => {
-    return (
-        <div style={{ height: "100vh", width: "100vw" }}>
-        <PrettyChatWindow
-          projectId = {'9ab22e81-4f44-4d43-9cd3-a344986c6ca4'}
-          username = { props.user.username }
-          secret = { props.user.secret }
-          style = {{ height: '100%' }}
-        />
-        </div>
-      );
-  };
-  export default ChatsPage;
+  const chatProps = useMultiChatLogic(
+    "9ab22e81-4f44-4d43-9cd3-a344986c6ca4",
+    props.user.username,
+    props.user.username
+  );
+  return (
+    <div style={{ height: "100%", width: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+
+  //   return (
+  //     <div style={{ height: "100%", width: "100%" }}>
+  //       <PrettyChatWindow
+  //         projectID="9ab22e81-4f44-4d43-9cd3-a344986c6ca4"
+  //         username={props.user.username}
+  //         secret={props.user.username}
+  //         style={{ height: "100%" }}
+  //       />
+  //     </div>
+  //   );
+};
+export default ChatsPage;
